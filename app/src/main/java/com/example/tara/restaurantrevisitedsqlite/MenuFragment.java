bbc394ac69a97c2ff90f419242edeb43583887ec
taraere <taraelsen@gmail.com>
2017-12-01 10:42:05 +0100

    clean up and menu fragment

--- app/src/main/java/com/example/tara/restaurantrevisitedsqlite/MenuFragment.java
+++ app/src/main/java/com/example/tara/restaurantrevisitedsqlite/MenuFragment.java
@@ -1,16 +1,14 @@
 package com.example.tara.restaurantrevisitedsqlite;
 
-
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,22 +24,22 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class CategoriesFragment extends ListFragment {
+public class MenuFragment extends Fragment {
 
-    private String TAG = "Error";
-    private List<String> foodCategories = new ArrayList<>();
+    List<String> foodPlatters = new ArrayList<>();
+    JSONObject c;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String url = "https://resto.mprog.nl/categories";
+
+        final Bundle arguments = this.getArguments();
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this.getContext());
+        String url = "https://resto.mprog.nl/menu";
 
         // request string response from url
         JsonObjectRequest stringRequest = new JsonObjectRequest(
@@ -50,67 +48,39 @@ public class CategoriesFragment extends ListFragment {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONArray courses = response.getJSONArray("categories");
+                            JSONArray courses = response.getJSONArray("items");
 
+                            //foodCourses = new String[courses.length()];
                             for (int i = 0; i < courses.length(); i++) {
-                                String sCourse = courses.getString(i);
-                                // add to array of courses
-                                foodCategories.add(sCourse);
+
+                                c = courses.getJSONObject(i);
+                                String sCategory = c.getString("category");
+
+                                if (sCategory.equals(arguments)){
+
+                                    String sName = c.getString("name");
+                                    foodPlatters.add(sName);
+                                }
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println("No internet connection");
                 Log.d(TAG, "onErrorResponse: No internet connection");
             }
         });
 
         // add request to RequestQueue
         queue.add(stringRequest);
-
-        // add adapter to adapt categories into views
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this.getContext(),
-                android.R.layout.simple_list_item_1,
-                foodCategories);
-        this.setListAdapter(adapter);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_categories, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_menu, container, false);
     }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        MenuFragment menuFragment = new MenuFragment();
-        String s = foodCategories.get(position);
-
-        Bundle args = new Bundle();
-        args.putString("category", s);
-        menuFragment.setArguments(args);
-
-        getFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container, menuFragment)
-                .addToBackStack(null)
-                .commit();
-    }
-
-    public List<String> getFoodCategories() {
-        return foodCategories;
-    }
-
 }
